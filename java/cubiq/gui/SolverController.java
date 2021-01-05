0ad4f21aadb91b552377c32d6a3d89d3e972208c
Tim Zech <zech.home@gmail.com>
2021-01-05 22:29:36 +0100

    Animation der Timeline
    Läuft nicht

--- java/cubiq/gui/SolverController.java
+++ java/cubiq/gui/SolverController.java
@@ -1,11 +1,16 @@
 package cubiq.gui;
 
 import cubiq.models.GuiModel;
+import javafx.animation.*;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -18,6 +23,8 @@ public class SolverController implements Observer {
 
     @FXML
     private HBox solveIconPane;
+    @FXML
+    private Button startButton;
 
     public SolverController() {
         imagePath = "/assets/solveIcons/";
@@ -46,6 +53,21 @@ public class SolverController implements Observer {
         for(int i = 0; i < solution.size(); i++) {
             solveIconPane.getChildren().add(new SolveIcon(solution.get(i)));
         }
+    }
+    @FXML
+    private void startSolution() {
+        Timeline startSolutionAnimation = new Timeline();
+        startSolutionAnimation.getKeyFrames().addAll(                                                       // O     R     U     L
+                new KeyFrame(new Duration(10), new KeyValue(solveIconPane.paddingProperty(), new Insets(0 ,0,0,0), Interpolator.EASE_BOTH)),
+                new KeyFrame(new Duration(10), new KeyValue(solveIconPane.paddingProperty(), new Insets(0 ,0,0, -149), Interpolator.EASE_BOTH))
+        );
+        startSolutionAnimation.setCycleCount(solution.size());
+        startSolutionAnimation.setRate(1);
+        solveIconPane.setVisible(true);
+        startSolutionAnimation.setOnFinished(e -> {
+            System.out.println("Penis");
+        });
+        startSolutionAnimation.play();
     }
 
     class SolveIcon extends ImageView {
